c58d457bccb9639d87290f33fd4f1b2e43fe4a53
Heinz Knutzen <heinz.knutzen@dataport.de>
2020-07-08 15:22:54 +0200

    Write back to file and recurse into directories.

--- main.go
+++ main.go
@@ -2,26 +2,43 @@ package main
 
 import (
 	"fmt"
+	"github.com/hknutzen/Netspoc/go/pkg/abort"
+	"github.com/hknutzen/Netspoc/go/pkg/conf"
+	"github.com/hknutzen/Netspoc/go/pkg/filetree"
 	"github.com/hknutzen/spoc-parser/parser"
 	"github.com/hknutzen/spoc-parser/printer"
-	"io/ioutil"
 	"os"
 )
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage:\n  %s filename\n", os.Args[0])
-		os.Exit(1)
-	}
-	path := os.Args[1]
-	bytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: Can't %s\n", err)
-		os.Exit(1)
-	}
+func processInput(input *filetree.Context) {
+	bytes := []byte(input.Data)
+	path := input.Path
 	list := parser.ParseFile(bytes, path)
 	for _, n := range list {
 		n.Normalize()
 	}
-	printer.File(list, bytes)
+	copy := printer.File(list, bytes)
+	err := os.Remove(path)
+	if err != nil {
+		abort.Msg("Can't remove %s: %s", path, err)
+	}
+	file, err := os.Create(path)
+	if err != nil {
+		abort.Msg("Can't create %s: %s", path, err)
+	}
+	_, err = file.Write(copy)
+	if err != nil {
+		abort.Msg("Can't write to %s: %s", path, err)
+	}
+	file.Close()
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s FILE|DIR\n", os.Args[0])
+		os.Exit(1)
+	}
+	path := os.Args[1]
+	conf.ConfigFromArgsAndFile(nil, path)
+	filetree.Walk(path, processInput)
 }
